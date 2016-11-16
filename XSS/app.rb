13b731cc1d7cc88b3224818e6c5f26390837bf69
Kent Gruber <kgruber1@emich.edu>
2016-11-16 14:13:45 -0500

    Updated reflected css

--- XSS/app.rb
+++ XSS/app.rb
@@ -1,16 +1,21 @@
 require 'sinatra'
+require 'sinatra/cookies'
+
+enable  :sessions
 
 # Intended use:
-# GET /xss?stuff=foo
+# GET /session?stuff=foo
 
 # Vulnerable use:
-# GET /xss?stuff=<script>alert(" [ ╯ ' □']╯︵ ┻━┻ ) ")</script>"
+# GET /xss?stuff=<script>alert(" [ ╯ ' □']╯︵ ┻━┻ ) ")</script>
+# GET /session?stuff=<script>alert(document.cookie)</script>
 
 get '/xss' do
-  if params[:stuff] 
+  if params[:stuff]
+    response.set_cookie 'vulnerable', cookies["rack.session"]
     @stuff = params[:stuff]
   else
-    @stuff = "Nothing for xss?stuff=" 
+    @stuff = "Nothing."
   end
-  erb :reflected_xss 
+  erb :reflected_xss
 end
